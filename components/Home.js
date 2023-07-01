d3a76ff1398bc02fae25d01bee4214b67eca8910
pritam jana <pritamjana360@gmail.com>
2023-07-01 15:36:58 +0530

    solve dynamically add input field

--- components/Home.js
+++ components/Home.js
@@ -4,85 +4,85 @@ import Styles from "@/app/styles/home.module.css"
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai"
 import CartItem from './CartItem'
 function Home() {
-    const [product, setProduct] = useState({})
-    const [cart, setCart] = useState([])
+  
+
     const [subTotal, setSubTotal] = useState(0)
+
     const [cartList, setCartList] = useState([{ title: "", id: "", qty: "", price: "" }])
-    const onChange = (e) => {
-        setProduct({ ...product, [e.target.name]: e.target.value })
 
-        // console.log("Set product", product)
-    }
-    const handleClick = () => {
-        const inputTagArray = [...cartList, cartList[0]]
-        setCartList(inputTagArray)
-        setCart(cart.concat(product))
-
-
-    }
-    const addtoCart = (e) => {
-
-        // let myCart = cart;
-        let cardCode = Number(Date.now().toString())
-        // if(cardCode in cart){
-        //     newCart[cardCode] = {title,id,qty:1,price}
-        // }
-        // else{
-        //     newCart[cardCode] = {title,id,qty:1,price}
-        // }
-        e.preventDefault()
-        setCart(cart.concat(product))
-        localStorage.setItem(JSON.stringify(cardCode), JSON.stringify({ "productItem": cart},{"CardCode":cardCode}))
-        setCart([])
-        setProduct({})
-        setCartList([{ title: "", id: "", qty: "", price: "" }])
-
-
-        // console.log({ cardCode }, [{ product: cart.concat(product) }])
+    const handleChange = (index, event) => {
+        const values = [...cartList];
+        values[index][event.target.name] = event.target.value
+       
+        setCartList(values)
 
     }
 
-    const data = JSON.parse(localStorage.getItem("1688149106394"))
-    // console.log(data)
+const removeInputList = (index)=>{
+    const values = [...cartList];
+    values.splice(index,1);
+    setCartList(values)
+}
 
-    // console.log("Cart List", cartList)
-    // console.log("Cart", cart)
+const addInputList = ()=>{
+setCartList([...cartList,{ title: "", id: "", qty: "", price: "" }])
+}
+const handleSubmit = (e)=>{
+    e.preventDefault()
+    let cartCode = Number(Date.now().toString())
+    localStorage.setItem(JSON.stringify(cartCode), JSON.stringify({cartCode,cartList}))
+    console.log({cartCode, cartList})
 
+}
+const getData = (key)=>{
+    let data =JSON.parse(localStorage.getItem(key))
+    console.log("Cart data",data)
 
-
+}
     return (
         <div className={Styles.container}>
             <div className={Styles.leftSide}>
 
                 {/* What is Unhandled Runtime Error: Which return cart.map is not a function*/}
-                {cart.product && cart.product.map(element => {
+
+                { /*  {cart.product && cart.product.map(element => {
                     <CartItem product={element} key={element.cardCode} keyValue={element.cardCode} />
 
                 })}
+                */
+                }
+
+
             </div>
             <div className={Styles.rightSide}>
-                <form autoComplete='off' className={Styles.addCart}>
+                <form autoComplete='off' className={Styles.addCart} >
                     <h4 className={Styles.heading}>Buy Product</h4>
 
                     {/* Add dynamically input tag, which is write downn below */}
                     {cartList.map((singleProduct, index) => {
                         return (
                             <div className={Styles.productDetails} key={index}>
-                                <input type="text" className={Styles.input} name="title" id="" placeholder='Product name' onChange={onChange} />
-                                <input type="text" className={Styles.input} name="id" id="" placeholder='Product Id' onChange={onChange} />
-                                <input type="number" className={Styles.input} name="qty" id="" placeholder='Product quantity' onChange={onChange} />
-                                <input type="number" className={Styles.input} name="price" id="" placeholder='Product Price' onChange={onChange} />
-                                <AiOutlineMinusCircle className={Styles.addIcon} />
+                                <input type="text" className={Styles.input} name="title" id="" placeholder='Product name' onChange={event => handleChange(index, event)} value={singleProduct.title} required/>
+                                <input type="text" className={Styles.input} name="id" id="" placeholder='Product Id' onChange={event => handleChange(index, event)} value={singleProduct.id} required/>
+                                <input type="number" className={Styles.input} name="qty" id="" placeholder='Product quantity' onChange={event => handleChange(index, event)} value={singleProduct.qty} required/>
+                                <input type="number" className={Styles.input} name="price" id="" placeholder='Product Price' onChange={event => handleChange(index, event)} value={singleProduct.price} required/>
+                                <AiOutlineMinusCircle className={Styles.addIcon} onClick={()=>removeInputList()}/>
 
-                                {cartList.length - 1 === index && <AiOutlinePlusCircle className={Styles.addIcon} onClick={handleClick} />}
+                                
+
+                              
+                                    {cartList.length - 1 === index && <AiOutlinePlusCircle className={Styles.addIcon} onClick={addInputList}/>}
+                               
 
 
                             </div>
                         )
                     })}
-                    {/* On submit create an state which store all the created input tag's value   like {cardcode:Date.now().toString(), product:{{{ title: "product1", id: "1", qty: "1", price: "1" },{ title: "product2", id: "2", qty: "2", price: "3" }}}  */}
 
-                    <button type="submit" onClick={addtoCart}>Save</button> <br />
+                    <button type='submit' onClick={()=>handleSubmit}>Save</button> <br />
+                    <button type="button" onClick={()=>getData(1688205053723)}>Get Data</button>
+
+                    
                 </form>
 
             </div>
