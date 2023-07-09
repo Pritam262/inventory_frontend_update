5c690b3790cd4b5932d709fa67f19aa1dcd9dca3
pritam <pritamjana360@gmail.com>
2023-07-09 14:41:18 +0530

    V-1.0.5

--- components/productItem.js
+++ components/productItem.js
@@ -1,2 +1,29 @@
+"use client"
+const { useContext } = require("react");
+import Styles from "@/app/styles/productitem.module.css"
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai"
+import ProductContext from '@/app/context/ProductContext'
+const ProductItem = (props) => {
+    const context = useContext(ProductContext)
+    const { deleteProduct } = context;
+    const { product, keyValue } = props;
 
+    return (
+
+
+        <div className={Styles.card}>
+            <h2 className={Styles.title}>{product.title}</h2>
+            <p className={Styles.p_text}>Product Id: {product.id}</p>
+            <p className={Styles.p_text}>Available qty: {product.qty}</p>
+            <p className={Styles.p_text}>Product Unit: {product.unit}</p>
+            <p className={Styles.p_text}>Product Price: {product.price}</p>
+            <div>
+                <AiOutlineEdit /> <AiOutlineDelete onClick={() => { deleteProduct(keyValue) }} />
+
+            </div>
+        </div>
+
+
+    )
+}
 export default ProductItem
